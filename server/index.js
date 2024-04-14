0e09a1c2274281b75ddd275b32cb991d1e8a2b55
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2024-04-14 22:06:02 +0500

    done

--- server/index.js
+++ server/index.js
@@ -10,17 +10,49 @@
 // });
 
 
+// const http= require('http');
+// const fs= require('fs');
+
+// const  server = http.createServer((req, res)=>{
+//     fs.appendFile("log.txt", `New req recieve at ${Date.now()} and from ${req.headers.location} \n`,  (err)=>{
+//         if (err){ err}
+//         else{
+//             switch(req.url){
+//                 case "/": res.end("From /")
+//                 break
+//                 case "/about": res.end("From about")
+//                 break
+//                 default: res.end("404")
+//         }
+// }})
+
+
+    
+    
+    
+// })
+
+// server.listen(8000, ()=>{console.log(`my server is started at ${Date.now()}`)})
+
+
+
+
+
+
+
 const http= require('http');
 const fs= require('fs');
+const url = require('url');
 
 const  server = http.createServer((req, res)=>{
+    const myurl = url.parse(req.url, true)
     fs.appendFile("log.txt", `New req recieve at ${Date.now()} and from ${req.headers.location} \n`,  (err)=>{
         if (err){ err}
         else{
-            switch(req.url){
-                case "/": res.end("From /")
+            switch(myurl.pathname){
+                case "/": res.end(`From / `)
                 break
-                case "/about": res.end("From about")
+                case "/about": res.end(`From about+ ${myurl.query.name}`)
                 break
                 default: res.end("404")
         }
@@ -33,3 +65,8 @@ const  server = http.createServer((req, res)=>{
 })
 
 server.listen(8000, ()=>{console.log(`my server is started at ${Date.now()}`)})
+
+
+
+
+
