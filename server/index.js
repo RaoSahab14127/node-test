5ce101b6f79649a2738757e6ba586f523cfa9c10
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2024-04-14 21:16:28 +0500

    make some progress

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
-const http = require('http')
-const server= http.createServer((req, res)=>{
-    console.log(new Date().getDate().toLocaleString())
-    res.end("Hello");
+// const http = require('http')
+// const server= http.createServer((req, res)=>{
+//     console.log(Date().now())
+//     res.end("Hello");
 
+// })
+
+// server.listen(8000, ()=>{
+//     console.log("Myserver is started")
+// });
+
+
+const http= require('http');
+const fs= require('fs');
+
+const  server = http.createServer((req, res)=>{
+    fs.appendFile("log.txt", `New req recieve at ${Date.now()} and from ${req.headers.location} \n`,  (err)=>{
+        if (err){ err}
+        else{
+            switch(req.url){
+                case "/": res.end("From /")
+                break
+                case "/about": res.end("From about")
+                break
+                default: res.end("404")
+        }
+}})
+
+
+    
+    
+    
 })
 
-server.listen(8000, ()=>{
-    console.log("Myserver is started")
-});
+server.listen(8000, ()=>{console.log(`my server is started at ${Date.now()}`)})
